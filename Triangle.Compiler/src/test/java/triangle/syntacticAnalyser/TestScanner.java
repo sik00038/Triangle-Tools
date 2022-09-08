07b0d85bdfe45194b90efec32deb4c9d7df0168f
Sandy Brownlee <sbr@cs.stir.ac.uk>
2022-09-08 17:57:48 +0100

    added test

---
+++ Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
@@ -0,0 +1,14 @@
+package triangle.syntacticAnalyser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestScanner {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
