f4e1a08f6192ccc3ae874d3d899a7b0fe1e01cab
Sandy Brownlee <sbr@cs.stir.ac.uk>
2022-09-09 17:08:30 +0100

    minor fix to tests

--- Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
+++ Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
@@ -33,8 +33,8 @@ public class TestScanner {
 	
 
 	@Test
-	public void testPoundDemo() {
-		compileExpectFailure("/pounddemo.tri");
+	public void testBarDemo() {
+		compileExpectFailure("/bardemo.tri");
 	}
 	
 
