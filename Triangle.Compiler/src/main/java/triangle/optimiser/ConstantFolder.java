ce307a11a6c71de9c69bd0a4c91205bb6437f6e9
Sandy Brownlee <sandybrownlee@users.noreply.github.com>
2022-10-06 17:20:59 +0100

    Update ConstantFolder.java

--- Triangle.Compiler/src/main/java/triangle/optimiser/ConstantFolder.java
+++ Triangle.Compiler/src/main/java/triangle/optimiser/ConstantFolder.java
@@ -19,6 +19,7 @@ import triangle.abstractSyntaxTrees.commands.CallCommand;
 import triangle.abstractSyntaxTrees.commands.EmptyCommand;
 import triangle.abstractSyntaxTrees.commands.IfCommand;
 import triangle.abstractSyntaxTrees.commands.LetCommand;
+import triangle.abstractSyntaxTrees.commands.RepeatCommand;
 import triangle.abstractSyntaxTrees.commands.SequentialCommand;
 import triangle.abstractSyntaxTrees.commands.WhileCommand;
 import triangle.abstractSyntaxTrees.declarations.BinaryOperatorDeclaration;
